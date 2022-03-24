5aabdb67cca4003bb09e743941c3dcc645f9b439
Muhammad Karim Fadillah <karim.fadillah@prosa.ai>
2022-03-24 14:30:25 +0700

    update commit

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from . import models
+from .post.database import engine
+from .post.router import post, user, comment, authentication
 
+app = FastAPI()
 
-app=FastAPI()
+models.Base.metadata.create_all(engine)
 
-@app.get('/')
-def index():
-        return {"message":"hallo"}
+app.include_router(authentication.router)
+app.include_router(post.router)
+app.include_router(user.router)
+app.include_router(comment.router)
