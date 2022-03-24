dd9138e28384baa22112f5131fa2e62b832dd3eb
Muhammad Karim Fadillah <karim.fadillah@prosa.ai>
2022-03-24 17:49:02 +0700

    update commit

--- post/config.py
+++ post/config.py
@@ -5,5 +5,5 @@ class DatabaseConfig(BaseSettings):
     url: str = "postgresql://postgres: @localhost/db_LandingPage"
 
     class Config:
-        env_previx = "database_"
+        env_prefix = "database_"
         env_file = ".env"
