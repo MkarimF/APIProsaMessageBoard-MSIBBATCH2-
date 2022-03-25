f72471a386e451dd97d0f064d09cb3b57a671135
Muhammad Karim Fadillah <karim.fadillah@prosa.ai>
2022-03-25 15:22:57 +0700

    updated commit

--- post/config.py
+++ post/config.py
@@ -1,9 +1,13 @@
-from pydantic import BaseSettings
+from pydantic import BaseSettings,validator
 
 
 class DatabaseConfig(BaseSettings):
     url: str = "postgresql://postgresql: @localhost/db_LandingPage"
 
+    @validator("url")
+    def fix_scheme(cls, v):
+        return v.replace("postgres://", "postgresql://")
+    
     class Config:
         env_prefix = "database_"
         env_file = ".env"
