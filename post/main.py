017dfb9ef7d4e00d2c8171f3c13c1215bfada597
Muhammad Karim Fadillah <karim.fadillah@prosa.ai>
2022-04-12 07:33:13 +0000

    Replace main.py untuk startup engine database

--- post/main.py
+++ post/main.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI
+import asyncio
+
+from sqlalchemy.exc import OperationalError
 from . import models
 from .database import engine
 from .router import post, user, comment, authentication
 
 app = FastAPI()
 
-models.Base.metadata.create_all(engine)
-
 app.include_router(authentication.router)
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(comment.router)
+
+@app.on_event("startup")
+async def startup():
+    success = False
+    while not success:
+        try:
+            models.Base.metadata.create_all(engine)
+        except OperationalError:
+            await asyncio.sleep(5.0)
+        else:
+            success = True
